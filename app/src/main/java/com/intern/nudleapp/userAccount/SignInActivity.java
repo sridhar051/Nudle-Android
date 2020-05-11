4380e14ab7e380d07d4acb4dcd7ea46e888b117a
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-05-11 20:26:01 +0530

    Created a Sign In page

--- app/src/main/java/com/intern/nudleapp/userAccount/SignInActivity.java
+++ app/src/main/java/com/intern/nudleapp/userAccount/SignInActivity.java
@@ -2,8 +2,10 @@ package com.intern.nudleapp.userAccount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.intern.nudleapp.R;
@@ -19,6 +21,12 @@ public class SignInActivity extends AppCompatActivity {
 
         text_SignUp = findViewById(R.id.textView_SignUp);
         text_SignUp.setPaintFlags(text_SignUp.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        text_SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+            }
+        });
 
     }
 }
