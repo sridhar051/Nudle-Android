1df751d38f3db514667265075dcd86a2e0d93217
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-05-15 23:06:35 +0530

    Added functionality to the widgets

--- app/src/main/java/com/intern/nudleapp/userAccount/SignInActivity.java
+++ app/src/main/java/com/intern/nudleapp/userAccount/SignInActivity.java
@@ -7,17 +7,24 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.intern.nudleapp.R;
 
 public class SignInActivity extends AppCompatActivity {
 
     TextView text_SignUp;
+    private TextInputLayout user_email, user_password;
+    private String inputEmail, inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        user_email = findViewById(R.id.user_email_SignIn);
+        user_password = findViewById(R.id.user_password_SignIn);
 
         text_SignUp = findViewById(R.id.textView_SignUp);
         text_SignUp.setPaintFlags(text_SignUp.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
@@ -28,5 +35,37 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private boolean validateEmail() {
+        inputEmail = user_email.getEditText().getText().toString().trim();
+        if(inputEmail.isEmpty()) {
+            user_email.setError("!Please type your Email");
+            return false;
+        } else {
+            user_email.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        inputPassword = user_password.getEditText().getText().toString().trim();
+        if(inputPassword.isEmpty()) {
+            user_password.setError("!Please type your password");
+            return false;
+        } else {
+            user_password.setError(null);
+            return true;
+        }
+    }
+
+    public void createSession(View v) {
+        if(!validateEmail() | !validatePassword())
+            return;
+
+        String input = "Email: " + user_email.getEditText().getText().toString().trim() + "\n";
+        input = input + "Password: " + user_password.getEditText().getText().toString().trim();
+
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
     }
 }
