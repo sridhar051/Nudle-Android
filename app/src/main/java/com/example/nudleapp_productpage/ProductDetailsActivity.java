062aca896478b2e10881d42c616c38ecbc12fbd1
makwanagopi <54862288+makwanagopi@users.noreply.github.com>
2020-05-19 03:48:08 +0530

    Add files via upload
    product page

---
+++ app/src/main/java/com/example/nudleapp_productpage/ProductDetailsActivity.java
@@ -0,0 +1,17 @@
+package com.example.nudleapp_productpage;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProductDetailsActivity extends AppCompatActivity {
+
+    private Toolbar mTopToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
