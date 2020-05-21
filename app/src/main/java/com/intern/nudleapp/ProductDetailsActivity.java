da78843e6cb569a1cf05252ca320b2e5fae53f6c
bughunter98 <saitejanaiduthota@gmail.com>
2020-05-22 00:40:34 +0530

    Cart Activity

--- app/src/main/java/com/intern/nudleapp/ProductDetailsActivity.java
+++ app/src/main/java/com/intern/nudleapp/ProductDetailsActivity.java
@@ -9,6 +9,6 @@ public class ProductDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_details);
+        setContentView(R.layout.product_images_layout);
     }
 }
