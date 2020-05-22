6c42d181b2f08ddb4b945672ddf772e2a1e8a670
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-05-22 12:25:35 +0530

    Created an interface to declare request methods

--- app/src/main/java/com/intern/nudleapp/NudleServices.java
+++ app/src/main/java/com/intern/nudleapp/NudleServices.java
@@ -1,4 +1,25 @@
 package com.intern.nudleapp;
 
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+import static com.intern.nudleapp.Constants.AUTH_URL;
+import static com.intern.nudleapp.Constants.REGISTER_URL;
+
 public interface NudleServices {
+
+    @GET(AUTH_URL)
+    Call<UserResponse> getUserDetails(@Query("email") String email);
+
+    @FormUrlEncoded
+    @POST(REGISTER_URL)
+    Call<UserResponse> postUserDetails(@Field("name") String name,
+                                       @Field("email") String email,
+                                       @Field("password") String password
+    );
+
 }
