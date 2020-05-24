5af0c89a0222635d41617a08f58ebcd635abdaf9
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-05-24 13:42:03 +0530

    Updated

--- app/src/main/java/com/intern/nudleapp/userAccount_fragment/SignUpActivity.java
+++ app/src/main/java/com/intern/nudleapp/userAccount_fragment/SignUpActivity.java
@@ -1,15 +1,12 @@
-package com.intern.nudleapp.userAccount;
+package com.intern.nudleapp.userAccount_fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
 import com.intern.nudleapp.APIClient;
 import com.intern.nudleapp.MainActivity;
