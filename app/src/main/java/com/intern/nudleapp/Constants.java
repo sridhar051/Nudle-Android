680b51b831d34eb36d3dd5d6a43b30475211ce79
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-05-24 13:39:22 +0530

    Changed the BASE URL

--- app/src/main/java/com/intern/nudleapp/Constants.java
+++ app/src/main/java/com/intern/nudleapp/Constants.java
@@ -2,8 +2,9 @@ package com.intern.nudleapp;
 
 public class Constants {
 
-    public static final String BASE_URL = "https://4f59b8ab.ngrok.io/services/api/";
+    public static final String BASE_URL = "http://customer.nudle.in/services/api/";
     public static final String AUTH_URL = "users/auth.php";
     public static final String REGISTER_URL = "users/register.php";
+
 
 }
