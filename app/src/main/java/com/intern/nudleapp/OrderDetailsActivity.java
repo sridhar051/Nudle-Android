cb6db51cb4741213f8068c9589ef863fc0b4e227
makwanagopi <makgopi007@gmail.com>
2020-05-26 18:13:51 +0530

    removed errors

--- app/src/main/java/com/intern/nudleapp/OrderDetailsActivity.java
+++ app/src/main/java/com/intern/nudleapp/OrderDetailsActivity.java
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
-
-import com.google.android.material.navigation.NavigationView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class OrderDetailsActivity extends AppCompatActivity {
 
@@ -16,4 +16,25 @@ public class OrderDetailsActivity extends AppCompatActivity {
         Toolbar mtool = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(mtool);
     }
+    public boolean OnCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.search_and_cart,menu);
+        return true;
+    }
+    public boolean OnOptionsItemSlected(MenuItem item){
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            finish();
+            return true;
+        }
+        if(id == R.id.main_search_icon){
+            //TODO search icon
+            return true;
+        }
+        if(id == R.id.main_cart_icon){
+            //TODO cart icon
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
