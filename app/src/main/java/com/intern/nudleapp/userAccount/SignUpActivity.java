03d8725842fdaaece6f86b7dbb2564dbfbc72342
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-05-28 13:39:04 +0530

    Updated

--- app/src/main/java/com/intern/nudleapp/userAccount/SignUpActivity.java
+++ app/src/main/java/com/intern/nudleapp/userAccount/SignUpActivity.java
@@ -1,28 +1,57 @@
 package com.intern.nudleapp.userAccount;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.credentials.Credential;
+import com.google.android.gms.auth.api.credentials.HintRequest;
+import com.google.android.gms.auth.api.phone.SmsRetriever;
+import com.google.android.gms.auth.api.phone.SmsRetrieverClient;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.intern.nudleapp.APIClient;
-import com.intern.nudleapp.MainActivity;
 import com.intern.nudleapp.NudleServices;
 import com.intern.nudleapp.R;
-import com.intern.nudleapp.UserResponse;
+import com.intern.nudleapp.models.UserResponse;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Random;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        OTPReceive, GoogleApiClient.OnConnectionFailedListener, OTPDialog.CustomDialogListener {
 
     private TextInputLayout user_name, user_email, user_password, user_confirmed_password, user_mobile;
     private String inputName, inputEmail, inputMobile, inputPassword, inputConfirmedPassword;
+    private GoogleApiClient mGoogleApiClient;
+    private int RESOLVE_HINT = 2;
+    OTPDialog mOTPDialog = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +63,15 @@ public class SignUpActivity extends AppCompatActivity {
         user_name = findViewById(R.id.user_name_SignUp);
         user_password = findViewById(R.id.user_password_SignUp);
         user_confirmed_password = findViewById(R.id.user_confirm_password_SignUp);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.CREDENTIALS_API)
+                .build();
+
+        getMobileNumber();
+
     }
 
     private boolean validateEmail() {
@@ -94,16 +132,52 @@ public class SignUpActivity extends AppCompatActivity {
         }
     }
 
-    public void registerUser(View v) {
+    public void verifyUser(View v) {
         if(!validateName() | !validateEmail() | !validateMobile() | !validatePassword() | !validateConfirmPassword())
             return;
 
-        startActivity(new Intent(this, MainActivity.class));
+      /*  int randomNumber = new Random().nextInt(999999);
+        Retrofit retrofit = APIClient.getRetrofitInstance();
+        NudleServices nudleServices = retrofit.create(NudleServices.class);
+        Call call = nudleServices.getOTP("T7pzDxlc2Oc-Sc4lHgnfkJca9KReGfcDgTCnfpjYdo", inputMobile, "Your otp : " + randomNumber,
+                "TXTLCL");
+
+        new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+
+                for (SmsMessage sms : messages) {
+                    String message = sms.getMessageBody();
+                    String otp = message.split(": ")[1];
+                    mOTPDialog = new OTPDialog(otp);
+                    mOTPDialog.show(getSupportFragmentManager(), "Dialog");
+
+                }
+
+            }
+        };
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) {
+
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+
+            }
+        }); */
+
+        //startSMSListener();
+
+        //startActivity(new Intent(this, MainActivity.class));
 
         // The following code is for registration into the database using retrofit
         // Do not make any changes in it as of now
 
-        Retrofit retrofit = APIClient.getRetrofitInstance();
+        /*Retrofit retrofit = APIClient.getRetrofitInstance();
         NudleServices nudleServices = retrofit.create(NudleServices.class);
 
         Call<UserResponse> call = nudleServices.postUserDetails(inputName, inputEmail, inputMobile, inputPassword);
@@ -123,14 +197,86 @@ public class SignUpActivity extends AppCompatActivity {
             public void onFailure(Call<UserResponse> call, Throwable t) {
 
             }
-        });
+        }); */
 
+    }
+
+    public void getMobileNumber() {
+        HintRequest hintRequest = new HintRequest.Builder()
+                .setPhoneNumberIdentifierSupported(true)
+                .build();
+
+        PendingIntent pendingIntent = Auth.CredentialsApi.getHintPickerIntent(mGoogleApiClient,
+                hintRequest);
+        try {
+            startIntentSenderForResult(pendingIntent.getIntentSender(), RESOLVE_HINT, null, 0, 0, 0);
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RESOLVE_HINT) {
+            if(resultCode == Activity.RESULT_OK) {
+                Credential credential = data.getParcelableExtra(Credential.EXTRA_KEY);
+                user_mobile.getEditText().setText(credential.getId());
+            }
+        }
+    }
+
+    public void startSMSListener() {
+        SmsRetrieverClient smsRetrieverClient = SmsRetriever.getClient(this);
+        Task<Void> mTask = smsRetrieverClient.startSmsRetriever();
+        mTask.addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(SignUpActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SignUpActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onOTPVerification() {
+        Toast.makeText(this, "Verified", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onOtpReceived(String otp) {
+
+    }
+
+    @Override
+    public void onOtpTimeout() {
+        Toast.makeText(this, "Time out, please resend", Toast.LENGTH_LONG).show();
     }
 
     public void onBackPress(View v) {
         onBackPressed();
     }
-
-
 
 }
