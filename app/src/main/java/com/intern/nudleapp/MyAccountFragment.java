7a2196c1e946da9a5a7a87522777f401338a7796
makwanagopi <makgopi007@gmail.com>
2020-06-01 17:40:01 +0530

    updated

--- app/src/main/java/com/intern/nudleapp/MyAccountFragment.java
+++ app/src/main/java/com/intern/nudleapp/MyAccountFragment.java
@@ -1,5 +1,6 @@
 package com.intern.nudleapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -17,12 +19,26 @@ public class MyAccountFragment extends Fragment {
     public MyAccountFragment() {
         // Required empty public constructor
     }
-
+    private Button viewAllAddressBtn;
+    public static final int MANAGE_ADDRESS =1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_account, container, false);
+        View view= inflater.inflate(R.layout.fragment_my_account, container, false);
+        viewAllAddressBtn = view.findViewById(R.id.view_all_addresses_btn);
+        viewAllAddressBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent myAddressesIntent = new Intent(getContext(),MyAddressesActivity.class);
+                myAddressesIntent.putExtra("MODE",MANAGE_ADDRESS);
+                getContext().startActivity(myAddressesIntent);
+
+            }
+        });
+
+        return view;
     }
 }
