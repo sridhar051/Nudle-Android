23ce21de08d46c6e3f727c128ad00afd8f919f2f
Dharani-Kumar-Reddy <dharanikumarreddy23@gmail.com>
2020-06-03 23:20:13 +0530

    added isnetworkconnected() method

--- app/src/main/java/com/intern/nudleapp/SplashActivity.java
+++ app/src/main/java/com/intern/nudleapp/SplashActivity.java
@@ -1,8 +1,12 @@
 package com.intern.nudleapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,7 +14,29 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //test
-        startActivity(new Intent(this,MainActivity.class));
+        if(isNetworkConnected()){
+            //
+            startActivity(new Intent(this,MainActivity.class));
+        }
+        else
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("You're offline.Check your Connection.")
+                    .setCancelable(false)
+                    .setPositiveButton("EXIT", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            finish();
+
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.setTitle("Whoops!!");
+            alert.show();
+        }
+
+    }
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
 }
