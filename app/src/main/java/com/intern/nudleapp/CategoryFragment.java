a66bc877a8f5ebac9bb1208f5436181a3a3d4348
charitha9 <pchari2000@gmail.com>
2020-06-11 23:28:14 +0530

    updated category fragment

--- app/src/main/java/com/intern/nudleapp/CategoryFragment.java
+++ app/src/main/java/com/intern/nudleapp/CategoryFragment.java
@@ -1,11 +1,13 @@
 package com.intern.nudleapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.GridView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
@@ -16,82 +18,24 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
-
+import android.widget.AdapterView;
 import java.util.ArrayList;
 import java.util.List;
+import android.content.Intent;
 
 public class CategoryFragment extends Fragment {
-    RecyclerView recyclerView;
-    ProductAdapter adapter;
-    List<Product> productList;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_category, container, false);
-
-        productList = new ArrayList<>();
-
-        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
-        productList.add(
-                new Product(
-                        1,
-                        "women shopping",
-                        "sale",
-                        4.3,
-                        60000,
-                        R.drawable.womenfashion));
-
-        productList.add(
-                new Product(
-                        1,
-                        "men shopping",
-                        "girls",
-                        4.3,
-                        60000,
-                        R.drawable.menfashion));
-        productList.add(
-                new Product(
-                        1,
-                        "kids shopping",
-                        "boys",
-                        4.3,
-                        60000,
-                        R.drawable.kidsfashion));
-        productList.add(
-                new Product(
-                        1,
-                        "jewellery shopping",
-                        "shoes",
-                        4.3,
-                        60000,
-                        R.drawable.jewellery));
-
-        productList.add(
-                new Product(
-                        1,
-                        "electronic items",
-                        "electronics",
-                        4.3,
-                        60000,
-                        R.drawable.shop2));
-
-
-
-
-        //creating recyclerview adapter
-        ProductAdapter adapter = new ProductAdapter(getContext(), productList, (ProductAdapter.OnProductListener) getContext());
-
-        //setting adapter to recyclerview
-        recyclerView.setAdapter(adapter);
-
-
-
+        GridView gridView = (GridView)view.findViewById(R.id.gridview);
+        gridView.setAdapter( new CategoryAdapter(getContext()));
 
         return view;
+
 
     }
 }
